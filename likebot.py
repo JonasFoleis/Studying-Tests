1116c89b1b2307a8a5bbb86b0e9429a48c11178d
JonasFoleis <jonas.foleis2@gmail.com>
2024-07-18 22:08:42 -0300

    instatest2

---
+++ likebot.py
@@ -0,0 +1,15 @@
+#1 - Navegar até o site https://www.instagram.com
+#2 - Entrar com o usuário
+#3 - Entrar com a senha
+#4 - Clicar em "Log In"
+#5 - Clicar em "Not now" para não salvar os dados no navegador.
+#6 - Fechar janela de "salvar senha"
+#7 - Pesquisar pela página desejada
+#8 - Entrar na págia
+#9 - Clicar na postagem mais recente
+#10 - Verificar se o post ja foi curtido ou não
+#11 - Caso tenha curtido o post, não fazer nada, e pausar o bot por 24 horas
+#12 - Se não tiver curtido, curtid o post
+#13 - Se não tiver curtido, comentar a foto
+#14 - Pausar por 24 horas
+#15 - Após as 24 horas, rodar tudo de novo
