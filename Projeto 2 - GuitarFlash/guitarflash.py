afa1c335654dbba72af961b611ce59d4bc0c14dd
JonasFoleis <jonas.foleis2@gmail.com>
2024-07-29 20:26:46 -0300

    Projeto 2 - Start

---
+++ Projeto 2 - GuitarFlash/guitarflash.py
@@ -0,0 +1,5 @@
+# Projeto 2 - Zerar Game Guitar Flash com PyAutoGUI
+# Entender os passos manuais para depois criar o código Python.
+
+# Verificar se há um botão com a cor correspondente dentro do círculo
+# daquela cor.
