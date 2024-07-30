a5311c60a4f1d6b830dd8eec177139ee0b7f1088
JonasFoleis <jonas.foleis2@gmail.com>
2024-07-29 21:17:21 -0300

    Guitar Flash

--- Projeto 2 - GuitarFlash/guitarflash.py
+++ Projeto 2 - GuitarFlash/guitarflash.py
@@ -3,3 +3,19 @@
 
 # Verificar se há um botão com a cor correspondente dentro do círculo
 # daquela cor.
+import pyautogui
+import keyboard
+from time import sleep
+
+while keyboard.is_pressed('1') == False:
+    if pyautogui.pixelMatchesColor(768,779,(0,152,0)):
+        pyautogui.press('a')
+        sleep(0.05)
+
+    if pyautogui.pixelMatchesColor(858,879,(255,8,8)):
+        pyautogui.press('s')
+        sleep(0.05)
+
+    if pyautogui.pixelMatchesColor(949,778,(244,244,2)):
+        pyautogui.press('j')
+        sleep(0.05)
